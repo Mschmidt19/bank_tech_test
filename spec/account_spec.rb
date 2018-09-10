2d0dde01a3753977e31de237d93bb83dc3667ad6
Marek <MarekSchmidt@outlook.com>
2018-09-10 11:30:08 +0100

    Account.transactions initializes as an empty array

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -7,9 +7,18 @@ describe Account do
   describe "#balance" do
     context "upon initialization" do
       it "should initialize with a balance of 0" do
-        expect(account.balance).to eq 0
+        expect(account.balance).to be 0
       end
     end
   end
+
+  describe "#transactions" do
+    context "upon initialization" do
+      it "should initialize as an empty array" do
+        expect(account.transactions).to eq []
+      end
+    end
+  end
+
 
 end
