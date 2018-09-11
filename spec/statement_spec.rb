156b472ce96e30ee0170b0b9d2a4b971f8fa7243
Marek <MarekSchmidt@outlook.com>
2018-09-11 18:02:21 +0100

    Added a test to statement_spec.rb to test for varrying column width of printed table

--- spec/statement_spec.rb
+++ spec/statement_spec.rb
@@ -4,10 +4,12 @@ describe Statement do
   let(:statement) { described_class.new }
   let(:mock_transaction1) { double :transaction }
   let(:mock_transaction2) { double :transaction }
+  let(:mock_transaction3) { double :transaction }
   let(:time) { Time.now }
 
   before do
     allow(Time).to receive(:now).and_return(time)
+
     allow(mock_transaction1).to receive(:date).and_return(time)
     allow(mock_transaction1).to receive(:credit).and_return(100)
     allow(mock_transaction1).to receive(:debit).and_return(nil)
@@ -17,6 +19,12 @@ describe Statement do
     allow(mock_transaction2).to receive(:credit).and_return(nil)
     allow(mock_transaction2).to receive(:debit).and_return(49.99)
     allow(mock_transaction2).to receive(:balance).and_return(50.01)
+
+    allow(mock_transaction3).to receive(:date).and_return(time)
+    allow(mock_transaction3).to receive(:credit).and_return(100_000_000_000)
+    allow(mock_transaction3).to receive(:debit).and_return(nil)
+    allow(mock_transaction3).to receive(:balance).and_return(100_000_000_000)
+
   end
 
   describe "#print_statement" do
@@ -36,6 +44,11 @@ describe Statement do
       it "Prints a formatted table of previous transactions" do
         expected_output = "#{'Date'.center(12)}||#{'Credit'.center(12)}||#{'Debit'.center(12)}||#{'Balance'.center(12)}\n#{time.strftime("%d/%m/%Y").center(12)}||#{''.center(12)}||#{'49.99'.center(12)}||#{'50.01'.center(12)}\n#{time.strftime("%d/%m/%Y").center(12)}||#{'100.00'.center(12)}||#{''.center(12)}||#{'100.00'.center(12)}\n"
         mock_transactions = [mock_transaction1, mock_transaction2]
+        expect { statement.print_statement(mock_transactions) }.to output(expected_output).to_stdout
+      end
+      it "Adjusts table column widths to the largest bit of data" do
+        expected_output = "#{'Date'.center(17)}||#{'Credit'.center(17)}||#{'Debit'.center(17)}||#{'Balance'.center(17)}\n#{time.strftime("%d/%m/%Y").center(17)}||#{'100000000000.00'.center(17)}||#{''.center(17)}||#{'100000000000.00'.center(17)}\n"
+        mock_transactions = [mock_transaction3]
         expect { statement.print_statement(mock_transactions) }.to output(expected_output).to_stdout
       end
     end
